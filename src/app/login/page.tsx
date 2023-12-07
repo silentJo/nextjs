333d1e9cc86b8a3135c44f087ddae9cc21595d1f
Jonathan CASIER <jonathancasier@gmail.com>
2023-12-07 15:47:39 +0100

    login & logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,19 @@
 import LoginForm from '@/app/ui/login-form';
+import Image from 'next/image'
 
 export default function LoginPage() {
     return (
         <main className="flex items-center justify-center md:h-screen">
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-                <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-                    <div className="w-32 text-white md:w-36">
-                        Snappies
-                    </div>
-                </div>
+                <Image
+                    src="/img-logo.png"
+                    width={200}
+                    height={200}
+                    alt="logo snappies"
+                    style={{
+                        margin: "auto",
+                    }}
+                />
                 <LoginForm />
             </div>
         </main>
--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -17,7 +17,7 @@ export default function LoginForm() {
         <form action={dispatch} className="space-y-3">
             <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
                 <h1 className={`mb-3 text-2xl text-black`}>
-                    Please log in to continue.
+                    Connexion
                 </h1>
                 <div className="w-full">
                     <div>
@@ -82,7 +82,7 @@ function LoginButton() {
     const {pending} = useFormStatus();
     return (
         <Button className="mt-4 w-full" aria-disabled={pending}>
-            Connection <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+            Se connecter <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
     );
 }
